c2f11fc391153f8d864665183d04aa3ac8aea477
Emir Kucukosman <m.emirkucukosman@gmail.com>
2023-12-04 18:19:04 +0300

    added websockets

--- index.ts
+++ index.ts
@@ -1,31 +1,18 @@
-import { http } from "@ampt/sdk";
+import { http, ws, SocketConnection } from "@ampt/sdk";
 import express, { Router } from "express";
 
 const app = express();
 
 const api = Router();
 
-api.get("/hello", (req, res) => {
-  return res.status(200).send({ message: "Hello from the public api!" });
-});
-
-api.get("/greet/:name", (req, res) => {
-  const { name } = req.params;
-
-  if (!name) {
-    return res.status(400).send({ message: "Missing route param for `name`!" });
-  }
-
-  return res.status(200).send({ message: `Hello ${name}!` });
-});
-
-api.post("/submit", async (req, res) => {
-  return res.status(200).send({
-    body: req.body,
-    message: "You just posted data",
-  });
+api.get("/", (req, res) => {
+  return res.status(200).send({ success: true });
 });
 
 app.use("/api", api);
+
+ws.on("connected", (socket: SocketConnection) => {
+  console.log("Socket connected", socket.connectionId);
+});
 
 http.node.use(app);
